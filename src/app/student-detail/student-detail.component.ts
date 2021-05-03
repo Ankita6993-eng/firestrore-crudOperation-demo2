595ac88dc2b017ec5552ed1df91d41bacba5227a
Ankita6993-eng <ankitaa.tagline@gmail.com>
2021-05-03 15:01:08 +0530

    first

--- src/app/student-detail/student-detail.component.ts
+++ src/app/student-detail/student-detail.component.ts
@@ -14,6 +14,7 @@ export class StudentDetailComponent implements OnInit {
   constructor(public studentService: StudentService,public firestore: AngularFirestore) { }
 students:any=[];
 stud:any;
+issubmit:boolean=false;
   ngOnInit(): void {
    this.studentService.getstudents().subscribe((list:any)=>{
      this.students=list.map((item:any)=>{
@@ -25,32 +26,41 @@ stud:any;
    })
   }
 
-  addstudents(data:any){ this.students.push(data)};
+  addstudents(data:any){ 
+    this.students.push(data)
+    };
 
-  onSubmit() {
+  createstud() {
+     this.issubmit=false
       this.studentService.createstudent(this.studentService.form.value)
       alert("Recored inserted...") 
       this.studentService.form.reset()
   }
 
-
-  
-  getstudent(){ 
+  getstudent(){
     this.studentService
       .getstudents()
       .subscribe(res => (this.stud = res));
   }   
 
   deletestudent(data:any) {
+     this.issubmit=false
     if(confirm("Are you sure to delete this recored")){
       this.studentService.deletestudents(data)
     }
     //this.studentService.deletestudents(data);
     }
   updaterecorde(){
+    this.issubmit=true
      this.studentService.updatestudent(this.studentService.form.value)
       alert("Recored Updated...") 
       this.studentService.form.reset()
+       this.issubmit=false
+  }
+
+  populateForm1(data){
+   this.studentService.form.setValue(data)
+   this.issubmit=true
   }
 
 }
